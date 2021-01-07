d0f8d997588c3fe7b9a3e06edd9ad98a4ecd0d55
Crowdbotics <team@crowdbotics.com>
2021-01-07 13:09:21 +0000

    Initial commit.

---
+++ ios/rntest_23713Tests/rntest_23713Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface rntest_23713 : XCTestCase
+
+@end
+
+@implementation rntest_23713
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
